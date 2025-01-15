89728e9feef09e360c943834985718e5b136125a
musfikanoni <musfikanoni@gmail.com>
2025-01-15 22:26:23 +0600

    File setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "flowbite-react"
+
+function App() {
+
+  return (
+    <>
+      <Button className="hover:bg-slate-400 text-white" gradientMonochrome="lime">Lime</Button>
+
+    </>
+  )
+}
+
+export default App
