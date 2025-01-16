5a6c0055fd9775303ebd4f2735c617f9337afad2
musfikanoni <musfikanoni@gmail.com>
2025-01-16 20:08:37 +0600

    Banner

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import img1 from '../../../assets/Home/banner-1.webp';
+import img2 from '../../../assets/Home/banner-2.jpg';
+import img3 from '../../../assets/Home/Banner-3.jpg';
+
+
+
+const Banner = () => {
+    return (
+        <div>
+            <Carousel  autoPlay interval={3000} infiniteLoop showThumbs={false} showStatus={false}>
+                <div className="h-[100vh]">
+                    <img src={img1} />
+                </div>
+                <div className="h-[100vh]">
+                    <img src={img2} />
+                </div>
+                <div className="h-[100vh]">
+                    <img src={img3} /> 
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+
+
+
+export default Banner;
