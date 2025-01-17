3108aeb4db8417f1d4537b68d176995d6a49fb93
musfikanoni <musfikanoni@gmail.com>
2025-01-17 13:50:01 +0600

    Footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../pages/Shared/NavBar/NavBar';
+import SiteFooter from '../pages/Shared/SiteFooter/SiteFooter';
+
 
 const Main = () => {
     return (
         <div>
             <NavBar></NavBar>
             <Outlet></Outlet>
+            <SiteFooter></SiteFooter>
         </div>
     );
 };
